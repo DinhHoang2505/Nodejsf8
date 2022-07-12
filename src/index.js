efb607d15683486c4317aeee2562b283d662eec5
HoangDV16 <hoangdv16@fsoft.com>
2022-07-12 21:45:51 +0700

    updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const exphbs = require('express-handlebars')
 const app = express()
 
+app.use(morgan('combined'))
+
+app.engine('hbs', exphbs.engine({
+  extname: 'hbs',
+}))
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
+
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.render('home')
 })
+
 
 app.listen(3000)
